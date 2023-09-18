2866cc023c7e610cf6baca3b53c4f5961d89d2bf
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-09-18 13:01:11 -0400

    created join function

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
+# import ipdb
+
+items = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
+
 def oxford_comma(items):
-    return None
+    if len(items) == 2:
+        return  ' and '.join(items)
+    elif len(items) >= 3:
+        return ', '.join(items[:-1]) + ', and ' + items[-1]
+    else:
+        return ", ".join(items)
